e736518171483757fe3aad2785951d1e7390a2ae
Blagovest Atanasov <blagovest.atanasovv@gmail.com>
2026-03-14 15:59:30 +0200

    Initialize Spring Boot bank wallet project

---
+++ src/test/java/com/wallet/bankwallet/BankWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.bankwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
